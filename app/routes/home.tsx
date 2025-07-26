7cbd053bf621cb4073097d9a9e5f2fab838f4cb4
xprofanity <pecpalmplay@gmail.com>
2025-07-26 12:57:48 +0200

    made homepage

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,11 +3,20 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResumeIQ" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback.</h2>
+        </div>
+      </section>
+    </main>
+  );
 }
